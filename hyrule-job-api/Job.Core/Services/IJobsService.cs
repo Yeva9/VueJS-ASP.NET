1550d6de5e01a7977e636570ff1c0a411e0d2c9e
Yeva <eva.hovnanyan.1999@gmail.com>
2023-11-13 18:20:34 +0400

    API: Added draft JobsService

---
+++ hyrule-job-api/Job.Core/Services/IJobsService.cs
@@ -0,0 +1,17 @@
+﻿using Job.Core.DTO;
+
+namespace Job.Core.Services
+{
+    /// <summary>
+    /// Jobs management
+    /// </summary>
+    public interface IJobsService
+    {
+        Task<List<JobDto>> GetAllJobs();
+        Task<JobDto> GetJob(int id);
+        Task<int> CreateJob(JobDto job);
+        Task UpdateJob(int id, JobDto job);
+        Task<JobDto> DeleteJob(int id);
+        Task<List<JobDto>> DeleteJobs(int[] ids);
+    }
+}
---
+++ hyrule-job-api/Job.Services/Services/JobsService.cs
@@ -0,0 +1,39 @@
+﻿using Job.Core.DTO;
+using Job.Core.Services;
+
+namespace Job.Services.Services
+{
+    //TBD
+    public class JobsService : IJobsService
+    {
+        public Task<int> CreateJob(JobDto job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JobDto> DeleteJob(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<JobDto>> DeleteJobs(int[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<JobDto>> GetAllJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JobDto> GetJob(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateJob(int id, JobDto job)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
