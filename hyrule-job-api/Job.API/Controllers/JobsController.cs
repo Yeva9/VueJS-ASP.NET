b56f88b088d92eac6b24b589dc66b0554023ce69
Yeva <eva.hovnanyan.1999@gmail.com>
2023-11-13 13:11:52 +0400

    API: Added CRUD APIs

--- hyrule-job-api/Job.API/Controllers/JobsController.cs
+++ hyrule-job-api/Job.API/Controllers/JobsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Job.Core.DTO;
+using Job.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Job.API.Controllers
 {
@@ -7,10 +10,119 @@ namespace Job.API.Controllers
     [ApiController]
     public class JobsController : ControllerBase
     {
-        [HttpGet]
-        public void GetJobs()
-        {
+        private readonly JobContext _context;
+        private readonly IMapper _mapper;
 
+        public JobsController(
+            JobContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllJobs()
+        {
+            return Ok(await _context.Jobs.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+            return Ok(job);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JobDto>> PostJob(JobDto job)
+        {
+            /*
+            if (!ModelState.IsValid) {
+                return BadRequest();
+            }
+            */
+            var jobModel = _mapper.Map<JobDto, Core.Models.Job>(job);
+            _context.Jobs.Add(jobModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "PostJob",
+                new { id = job.Id },
+                job);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutJob(int id, JobDto job)
+        {
+            if (id != job.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(job).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Jobs.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Core.Models.Job>> DeleteJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return job;
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<ActionResult> DeleteMultiple([FromQuery] int[] ids)
+        {
+            var jobs = new List<Core.Models.Job>();
+            foreach (var id in ids)
+            {
+                var job = await _context.Jobs.FindAsync(id);
+
+                if (job == null)
+                {
+                    return NotFound();
+                }
+
+                jobs.Add(job);
+            }
+
+            _context.Jobs.RemoveRange(jobs);
+            await _context.SaveChangesAsync();
+
+            return Ok(jobs);
         }
     }
 }
---
+++ hyrule-job-api/Job.Core/DTO/JobDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Job.Core.DTO
+{
+    public class JobDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Loaction { get; set; } = string.Empty;
+        public int Salary { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public bool IsFav { get; set; }
+    }
+}
